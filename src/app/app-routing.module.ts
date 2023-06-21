a131ee6ce274f7ed2b257361a4aee13b90cb6378
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-21 06:05:23 -0300

    Adicionando rotas para os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { DespesasComponent } from './pages/despesas/despesas.component';
+import { ReceitasComponent } from './pages/receitas/receitas.component';
+import { LancamentosComponent } from './pages/lancamentos/lancamentos.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' }, 
-  { path: '/', component: LoginComponent }, 
-  { path: '/dashboard', component: LayoutComponent }, 
-  { path: '/logout', component: LoginComponent }, 
+  { path: 'login', component: LoginComponent }, 
+  { path: 'dashboard', component: DashboardComponent }, 
+  { path: 'categoria', component: CategoriasComponent }, 
+  { path: 'despesa', component: DespesasComponent }, 
+  { path: 'receita', component: ReceitasComponent }, 
+  { path: 'lancamento', component: LancamentosComponent }, 
+  { path: 'logout', component: LoginComponent }, 
 ];
 
 @NgModule({
