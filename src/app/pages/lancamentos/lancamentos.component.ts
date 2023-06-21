d38eef2bf0c27e20c22dedf27dd016d8947b0524
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-21 06:03:02 -0300

    Criação do Componente Lançamentos

---
+++ src/app/pages/lancamentos/lancamentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamentos',
+  templateUrl: './lancamentos.component.html',
+  styleUrls: ['./lancamentos.component.sass']
+})
+export class LancamentosComponent {
+
+}
