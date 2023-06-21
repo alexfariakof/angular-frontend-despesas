f9ad47fca8beff2f4683072614e98a71db88b907
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-21 03:51:32 -0300

    Configurações de rotas iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './shared/components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' }, 
+  { path: '/', component: LoginComponent }, 
+  { path: '/dashboard', component: LayoutComponent }, 
+  { path: '/logout', component: LoginComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
