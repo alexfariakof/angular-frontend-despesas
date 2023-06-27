b41bc3b3196e3cc18c379687b2ad69900776c49c
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-27 18:56:32 -0300

    removendo configurações de modulo do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { ILogin } from "src/app/shared/interfaces/ILogin";
 
 @Component({
@@ -8,10 +10,29 @@ import { ILogin } from "src/app/shared/interfaces/ILogin";
 })
 
 export class LoginComponent {
-  login: ILogin = { email: '', password: '' };
-  @Output() loginClicked: EventEmitter<void> = new EventEmitter<void>();
+    @Input() login: ILogin = { email: '', password: '' };  
+
+  constructor(public formbuilder: FormBuilder, 
+    private router: Router){
+
+  }
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void{
+    this.loginForm = this.formbuilder.group({
+        txtLogin: ['', [Validators.required, Validators.email]],
+        txtPassword: ['', Validators.required]
+    })
+    
+        
+  }
+
+  getLoginDados(){
+    return this, this.loginForm.controls;
+  }
 
   onLoginClick() {
-    this.loginClicked.emit();
+    alert('email: ' + this.login.email + ' -' + 'password: ' + this.login.password );    
   }
 }
