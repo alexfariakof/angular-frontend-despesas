7014c181b73f4213500f87e47b4a675db9607e39
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-27 23:37:51 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { ConfiguracoesComponent } from './pages/configuracoes/configuracoes.component';
 
 const routes: Routes = [
   { path: '',  pathMatch: 'full', redirectTo: 'login' }, 
   { path: 'login', component: LoginComponent }, 
-  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule),
-}, 
-  { path: '', component: LoginComponent }, 
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule), }, 
+  { path: 'categoria', loadChildren: () => import('./pages/categorias/categorias.module').then(m => m.CategoriasdModule), },
+  { path: 'despesa', loadChildren: () => import('./pages/despesas/despesas.module').then(m => m.DespesasModule), },
+  { path: 'receita', loadChildren: () => import('./pages/receitas/receitas.module').then(m => m.ReceitasModule), },
+  { path: 'lancamento', loadChildren: () => import('./pages/lancamentos/lancamentos.module').then(m => m.LancamentosModule),}
 ];
 
 @NgModule({
