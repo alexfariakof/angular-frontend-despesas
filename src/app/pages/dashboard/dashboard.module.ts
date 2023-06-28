e4fbd0c970153fba8861c87caa70823339032cb1
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-28 01:37:47 -0300

    Implementação  do componente bar-chart e adicionando ao dashboard

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { BarChartComponent } from 'src/app/shared/components/bar-chart/bar-chart.component';
+import { NgChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   providers: [],
-  declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, SharedModule]
+  declarations: [DashboardComponent, BarChartComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule, NgChartsModule]
 })
 export class DashboardModule {}
