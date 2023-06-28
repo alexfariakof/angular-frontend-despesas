18396f3554e70a7f9361b8b4410808c24ff7cd1b
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-27 23:38:56 -0300

    Criação componente Categorias herdando layoutCaomponent

---
+++ src/app/pages/categorias/categorias.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriasComponent } from './categorias.component';
+import { CategoriaRoutingModule } from './categorias-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+
+
+@NgModule({
+  providers: [],
+  declarations: [CategoriasComponent ],
+  imports: [CommonModule, CategoriaRoutingModule, SharedModule]  
+})
+export class CategoriasdModule {}
