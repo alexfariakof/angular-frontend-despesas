ec3d15dd89789539e2d53f206d87d7c31f1dd947
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-09-25 20:53:22 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml', 'coverage'],
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     restartOnFileChange: true
   });
 };
