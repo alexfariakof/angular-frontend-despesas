42ba55262811a46db6199380997dd95dc1f27b56
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-06 20:44:01 -0300

    Implementação de Componente para Mensagens de Sucesso

--- src/app/shared/components/success-alert/success-alert.component.ts
+++ src/app/shared/components/success-alert/success-alert.component.ts
@@ -1,10 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-success-alert',
-  templateUrl: './success-alert.component.html'  
+  standalone: true,
+  templateUrl: './success-alert.component.html',
+  providers: [NgbModalConfig, NgbModal],
 })
 export class SuccessAlertComponent {
   @Input() message:string ='';
+
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+		config.backdrop = 'static';
+    config.keyboard = false;
+	}
+
+	open(content) {
+		this.modalService.open(content);
+	}
+
+  close(){
+    this.modalService.dismissAll();
+  }
 
 }
