d3e990d23c4a9ca4dd08f989cc6b08be2ba031a6
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-08 19:32:53 -0300

    Test Unit Implementation LoginComponent

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -9,7 +9,6 @@ import { LayoutRoutingModule } from 'src/app/shared/components/layout/layout-rou
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
-import { SuccessAlertComponent } from 'src/app/shared/components/success-alert/success-alert.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -17,7 +16,6 @@ describe('LoginComponent', () => {
   let loginService: LoginService;
   let router: Router;
   let modalService: NgbModal;
-
 
   beforeEach(() => {
     const loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
@@ -39,52 +37,68 @@ describe('LoginComponent', () => {
   });
 
   it('should create', () => {
+    // Assert
     expect(component).toBeTruthy();
   });
 
   it('should navigate to dashboard on successful login', () => {
+    // Arrange
     const login : ILogin = { email: 'teste@test.com', senha: 'teste' };
     (loginService.login as jasmine.Spy).and.returnValue(of({ authenticated: true }));
-
     component.login = login;
+
+    // Act
     component.onLoginClick();
 
+    // Assert
     expect(router.navigate).toHaveBeenCalledWith(['/dashboard']);
   });
 
   it('should open a modal with a message', () => {
+    // Arrange
     const modalRef = jasmine.createSpyObj('NgbModalRef', ['componentInstance']);
     const message = 'Test message';
-
     (modalService.open as jasmine.Spy).and.returnValue(modalRef);
 
+    // Act
     component.showMessage(message);
 
+    // Assert
     expect(modalService.open).toHaveBeenCalledWith(WarningAlertComponent);
     expect(modalRef.componentInstance.message).toBe(message);
   });
 
   it('should return login form controls', () => {
+    // Arrange
     component.ngOnInit();
     component.loginForm.controls['txtLogin'].setValue('teste@teste.com');
     component.loginForm.controls['txtPassword'].setValue('password');
 
+    // Act
     const loginDados = component.getLoginDados;
+
+    // Assert
     expect(loginDados['txtLogin'].value).toBe('teste@teste.com');
     expect(loginDados['txtPassword'].value).toBe('password');
   });
 
   it('should toggle password visibility and update eye icon class', () => {
+    // Arrange
     component.ngOnInit();
-
     component.showPassword = false;
     component.eyeIconClass = 'bi-eye';
+
+    // Act
     component.onTooglePassword();
 
+    // Assert
     expect(component.showPassword).toBe(true);
     expect(component.eyeIconClass).toBe('bi-eye-slash');
 
+    // Act
     component.onTooglePassword();
+
+    // Assert
     expect(component.showPassword).toBe(false);
     expect(component.eyeIconClass).toBe('bi-eye');
   });
