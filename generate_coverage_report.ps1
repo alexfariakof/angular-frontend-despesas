3dc4771703c4c3a7cc41d05d38070bf47a79cfa0
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-08 19:31:10 -0300

    Implementation To Watch And Update Report Coverage Automaticaly

--- generate_coverage_report.ps1
+++ generate_coverage_report.ps1
@@ -1,28 +1,31 @@
 param(
-    [switch]$w
+    [switch]$w,
+    [switch]$d
 )
 
 # Pasta onde o relatório será gerado
 $reportPath = ".\coverage\lcov-report"
 
 if ($w) {
-  # Executa os testes unitários e gera Relatório Coverage
-  npm run test:coverage --watch
+  npm run test:coverage
+   # Encontra o diretório mais recente se for existente
+  $latestDir = Get-ChildItem -Directory -Path $reportPath | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
-} else {
-  # Executa os testes unitários em modo de observação (watch) sem gerar relatório de cobertura
-  npm run test
-}
-
-# Encontra o diretório mais recente se for existente
-$latestDir = Get-ChildItem -Directory -Path $reportPath | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-
-# Verifica se encontrou um diretório e, em caso afirmativo, obtém o nome do diretório (GUID)
-if ($latestDir -ne $null) {
-
+  # Verifica se encontrou um diretório e, em caso afirmativo, obtém o nome do diretório (GUID)
+  if ($latestDir -ne $null) {
     # Abre a página index.html no navegador padrão do sistema operacional
     Invoke-Item $reportPath\index.html
+    # Executa os testes unitários e gera Relatório Coverage em modo de observação
+    npm run test:watch
+  }
 }
 else {
-    Write-Host "Nenhum diretório de resultados encontrado."
+  if ($d) {
+    # Executa os testes unitários em modo de observação no Chrome com karma
+    npm run test:debug
+  }
+  else {
+    # Executa os testes unitários
+    npm run test
+  }
 }
