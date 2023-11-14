ada26c1d08e4293c74aeae64f03d7e4905e5b539
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2023-11-14 17:35:31 -0300

    Reopen Feature to Fix Problems Find in Sonar Cloud (#31)
    * bugfix/Removing Duplications Codes
    
    * bugfix/Resolving Issues Find by Sonar Cloud
    
    * bugfix/ Removing Duplications Codes
    
    * bugfix/ Removing Duplications Codes

--- src/app/pages/categorias/categorias-form/categorias.form.component.ts
+++ src/app/pages/categorias/categorias-form/categorias.form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AlertComponent } from 'src/app/shared/components/alert-component/alert.component';
 
--- src/app/pages/configuracoes/configuracoes.module.ts
+++ src/app/pages/configuracoes/configuracoes.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConfiguracoesComponent } from './configuracoes.component';
 import { ConfiguracoesRoutingModule } from './configuracoes-routing.module';
 @NgModule({
-  providers: [],
-  declarations: [ConfiguracoesComponent ],
   imports: [CommonModule, ConfiguracoesRoutingModule]
 })
 export class ConfiguracoesModule {}
