0188bd6b55c9842519ac3539c000dc589d84cadd
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-14 16:51:01 -0300

    bugfix/Removing Duplications Codes

--- src/app/pages/perfil/perfil.module.ts
+++ src/app/pages/perfil/perfil.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PerfilComponent } from './perfil.component';
 import { PerfilRoutingModule } from './perfil-routing.module';
 import { FormsModule } from '@angular/forms';
 @NgModule({
-  providers: [],
-  declarations: [PerfilComponent ],
   imports: [ CommonModule, FormsModule, PerfilRoutingModule ]
 })
 export class PerfilModule {}
