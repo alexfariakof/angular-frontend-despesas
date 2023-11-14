0582107f5a92dc4d17f112c42716570ce9230f67
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-14 18:07:27 -0300

    bugfix/Duplications Codes Find In Sonar Cloud

--- src/app/shared/components/layout/layout-routing.module.ts
+++ src/app/shared/components/layout/layout-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '',  pathMatch: 'full', redirectTo: 'dashboard' }];
+  { path: '',  pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'categoria', redirectTo: 'categoria' },
+  { path: 'despesa', redirectTo: 'despesa' },
+  { path: 'receita', redirectTo: 'receita' },
+  { path: 'lancamento', redirectTo: 'lancamento' },
+  { path: 'perfil', redirectTo: 'perfil' },
+  { path: 'configuracoes', redirectTo: 'configuracoes'}
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
