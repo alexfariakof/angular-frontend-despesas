afbd84b6075edbbd324ccaaae8878fa47a465a87
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-14 18:20:19 -0300

    bugfix/Duplications Codes Find In Sonar Cloud

--- src/app/shared/components/layout/layout-routing.module.ts
+++ src/app/shared/components/layout/layout-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '',  pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'categoria', redirectTo: 'categoria' },
-  { path: 'despesa', redirectTo: 'despesa' },
-  { path: 'receita', redirectTo: 'receita' },
-  { path: 'lancamento', redirectTo: 'lancamento' },
-  { path: 'perfil', redirectTo: 'perfil' },
-  { path: 'configuracoes', redirectTo: 'configuracoes'}
+  { path: 'dashboard', loadChildren: () => import('../../../pages/dashboard/dashboard.module').then(m => m.DashboardModule), },
+  { path: 'categoria', loadChildren: () => import('../../../pages/categorias/categorias.module').then(m => m.CategoriasdModule), },
+  { path: 'despesa', loadChildren: () => import('../../../pages/despesas/despesas.module').then(m => m.DespesasModule), },
+  { path: 'receita', loadChildren: () => import('../../../pages/receitas/receitas.module').then(m => m.ReceitasModule), },
+  { path: 'lancamento', loadChildren: () => import('../../../pages/lancamentos/lancamentos.module').then(m => m.LancamentosModule),},
+  { path: 'perfil', loadChildren: () => import('../../../pages/perfil/perfil.module').then(m => m.PerfilModule)},
+  { path: 'configuracoes', loadChildren: () => import('../../../pages/lancamentos/lancamentos.module').then(m => m.LancamentosModule)}
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
