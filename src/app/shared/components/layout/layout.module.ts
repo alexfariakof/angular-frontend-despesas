d466a103d9b7b533a2adb54388d76cdeed803c5b
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-14 17:50:17 -0300

    bugfix/Duplications Codes Find In Sonar Cloud

--- src/app/shared/components/layout/layout.module.ts
+++ src/app/shared/components/layout/layout.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
-
 @NgModule(
     {
-        providers:[],
-        declarations:[LayoutComponent],
-        imports:[
-            CommonModule,
-            LayoutRoutingModule
-        ]
+        imports:[CommonModule, LayoutRoutingModule]
     }
 )
 
