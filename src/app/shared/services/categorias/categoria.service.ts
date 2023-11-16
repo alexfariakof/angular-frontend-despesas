6fbd81e0dda09955453744f471f36dbb84e0ef0f
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-16 12:50:12 -0300

    Initial Commit Create Categorias Flow

---
+++ src/app/shared/services/categorias/categoria.service.ts
@@ -0,0 +1,15 @@
+  import { HttpClient } from '@angular/common/http';
+  import { Injectable } from '@angular/core';
+  import { environment } from '../../environments/environment';
+
+  @Injectable({
+      providedIn: 'root'
+  })
+
+  export class CategoriaService {
+
+    constructor(private httpClient: HttpClient) { }
+
+    private readonly baseUrl = environment["endPoint"];
+
+  }
