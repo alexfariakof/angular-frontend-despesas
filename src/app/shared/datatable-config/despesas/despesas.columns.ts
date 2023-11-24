33623426a2c8c6790b1f20e4828ad17c6583ab15
Ribeiro <alexfariakof@gmail.com>
2023-11-23 22:40:07 -0300

    Finish Implementation feature/listar-despesas-cadastradas

--- src/app/shared/datatable-config/despesas/despesas.columns.ts
+++ src/app/shared/datatable-config/despesas/despesas.columns.ts
@@ -1,6 +1,18 @@
 export const DespesaColumns = [
   {
+    title: 'Data',
+    data: 'data'
+  },
+  {
     title: 'Descrição',
     data: 'descricao'
+  },
+  {
+    title: 'Valor',
+    data: 'valor'
+  },
+  {
+    title: 'Data Venciemnto',
+    data: 'dataVencimento'
   }
 ]
